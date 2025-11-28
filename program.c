62e5f145e935a2345002d555ee92c3da70f0bca1
Gaia0704 <gaia.saly-reygrobellet@esme.fr>
2025-11-28 07:32:48 +0000

    main principal

--- program.c
+++ program.c
@@ -11,20 +11,20 @@ int main() {
     printf("                                        -------------\n");
     printf("Bonjour cher joueur, et bienvenu dans Multigame, une plateforme"
         " qui rassemble les meilleurs jeux du MONDE entier!!!\n Ici on est la pour se détendre...\n\n"
-        "Tout d'abord comment veux -tu etre appelé : ");
+        "Tout d'abord comment veux -tu etre appelé :\n ");
         fgets(nom_joueur, 100, stdin);
         nom_joueur[strlen(nom_joueur)-1]='\0';
         printf("\n");
         printf("%s, ca c'est bien un nom de winner! Tu sais un des plus grand joueur de Multigame a le meme nom que toi, coincidence je crois pas...\n\n", nom_joueur);
         printf("Alors %s sur cette plateforme, tu peux choisir de jouer entre 3 jeux :\n"
-        "- Devine le nombre \n- Bataille (le jeux de carte) \n- Le pendu\n",nom_joueur);
+        "- Devine le nombre \n- Bataille (le jeux de carte) \n- Le pendu\n\n",nom_joueur);
 
         printf("Est -ce que tu veux les consignes d'un jeu? Si oui choissiez un ou pluisieurs nombre entre :\n");
         int consignes_jeux;
             printf("1) Devine le nombre \n");
             printf("2) Bataille\n");
-            printf("3) Le pendu \n");
-            printf("Alors quelle(s) consigne(s) de jeux choisissez-vous ?");
+            printf("3) Le pendu \n\n");
+            printf("Alors quelle(s) consigne(s) de jeux choisissez-vous ?\n");
             scanf("%d", &consignes_jeux);
         
         int chiffre;
@@ -35,21 +35,21 @@ int main() {
 
         if (chiffre== 1)
         {
-            printf("Devine le nombre\n"
+            printf("\nDevine le nombre\n"
                 "L’ordinateur choisit un nombre secret entre 1 et 100. Ton but est de le deviner. Après chaque essai," 
                 "on te dit si le nombre secret est plus grand ou plus petit. Continue jusqu’à trouver le bon nombre !\n\n");
         }
 
         if (chiffre==2)
         {
-            printf("La Bataille\n"
+            printf("\nLa Bataille\n"
                 "Chaque joueur retourne la première carte de son paquet. Celui qui a la carte la plus forte gagne les cartes." 
                 "En cas d’égalité, c’est la bataille ! Le joueur qui remporte toutes les cartes gagne la partie.\n\n");
         }
 
         if (chiffre==3)
         {
-            printf("Le pendu\n"
+            printf("\nLe pendu\n"
                 "Devine le mot en proposant des lettres." 
                 "Chaque erreur ajoute une partie du pendu. Tu gagnes si tu trouves le mot avant que le dessin soit complet !\n\n");
         }
